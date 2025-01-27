7c6c7dd5a4b8ddb0a679275359ac4c9e2c20075d
purpurb <purpurblock9@gmail.com>
2025-01-27 21:20:38 +0300

    Create credentials.h

---
+++ src/main/credentials.h
@@ -0,0 +1,4 @@
+const char* ssid = "your WiFi network name";
+const char* password = "your WiFi network password";
+static const char* control_panel_passwd = "camera control panel password";
+static const char* control_panel_login = "camera control panel username";
